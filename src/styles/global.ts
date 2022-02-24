477b74f7e8ca8c7f51ff41cbfaf428122c8e9016
matheus <matheus.xmaz10@gmail.com>
2022-02-23 21:43:34 -0300

    fontes do google fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -26,6 +26,15 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
 
+  body, input, textarea, button{
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+
+  h1,h2,h3,h4,h5,h6, strong{
+    font-weight: 600;
+  }
+
   body{
     background: var(--background);
     --webkit-font-smoothing: antialiased
