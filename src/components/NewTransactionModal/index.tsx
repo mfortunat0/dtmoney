77ec6f7f664df33d08db91308209ebd09c82fafa
matheus <matheus.xmaz10@gmail.com>
2022-02-25 21:55:32 -0300

    salvando dados do form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import closeImage from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
@@ -15,6 +15,13 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   const [type, setType] = useState("deposit");
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+
+  function handleCreateNewTransactin(event: FormEvent) {
+    event.preventDefault();
+  }
 
   return (
     <Modal
@@ -30,10 +37,19 @@ export function NewTransactionModal({
       >
         <img src={closeImage} alt="Fechar Modal" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransactin}>
         <h2>Cadastrar transação</h2>
-        <input placeholder="Titulo" />
-        <input placeholder="Valor" type="number" />
+        <input
+          placeholder="Titulo"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          placeholder="Valor"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+          type="number"
+        />
         <TransactionTypeContainer>
           <RadioBox
             type="button"
@@ -54,7 +70,11 @@ export function NewTransactionModal({
             <span>Saida</span>
           </RadioBox>
         </TransactionTypeContainer>
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
