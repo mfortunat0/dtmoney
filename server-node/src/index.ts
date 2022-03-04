6a2ef1188440e99037f2aa5eb6d907ba8eb2e790
matheus <matheus.xmaz10@gmail.com>
2022-03-04 17:14:47 -0300

    init server

---
+++ server-node/src/index.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
