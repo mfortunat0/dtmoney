75b3b3e95cccffac9155820e6961a61a444b1d50
matheus <matheus.xmaz10@gmail.com>
2022-03-08 10:23:36 -0300

    remove miragejs dependency

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,48 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import { createServer, Model } from "miragejs";
-
-createServer({
-  models: {
-    transaction: Model,
-  },
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: "Freela webpage",
-          type: "deposit",
-          category: "Dev",
-          amount: 4500,
-          createdAt: new Date("2021-03-15 08:00:00"),
-        },
-
-        {
-          id: 2,
-          title: "Supermercado",
-          type: "withdraw",
-          category: "Compras",
-          amount: 1200,
-          createdAt: new Date("2021-03-15 13:00:00"),
-        },
-      ],
-    });
-  },
-  routes() {
-    this.namespace = "api";
-
-    this.get("/transactions", () => {
-      return this.schema.all("transaction");
-    });
-
-    this.post("/transactions", (schema, request) => {
-      const data = JSON.parse(request.requestBody);
-      return schema.create("transaction", data);
-    });
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
